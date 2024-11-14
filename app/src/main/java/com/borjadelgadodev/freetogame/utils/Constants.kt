aa7a66c0d6f4cc8553b90beb37afef3f07c33fee
Borja Delgado Alcolea <borjadelgadodev@gmail.com>
2024-11-14 11:08:13 +0100

    CodeRabbit

--- app/src/main/java/com/borjadelgadodev/freetogame/utils/Constants.kt
+++ app/src/main/java/com/borjadelgadodev/freetogame/utils/Constants.kt
@@ -4,4 +4,5 @@ object Constants {
     const val BASE_URL = "https://www.freetogame.com/api/"
     const val GAMES_ENDPOINT = "games"
     const val GAME_ID_ENDPOINT = "game"
+    const val PRUEBA = "No se usa "
 }
